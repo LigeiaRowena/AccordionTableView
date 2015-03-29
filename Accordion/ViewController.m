0ef7a38ee84d873c7e6b85b04522eee8db5551a7
Francesca Corsini <ladyonirya@gmail.com>
2015-03-29 15:56:50 +0200

    Primo commit

---
+++ Accordion/ViewController.m
@@ -0,0 +1,61 @@
+
+//
+//  ViewController.m
+//  Accordion
+//
+//  Created by Francesca Corsini on 03/03/15.
+//  Copyright (c) 2015 Francesca Corsini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+#pragma mark - Init
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+	
+	//self.accordionView.animation = AccordionAnimationNone;
+	//self.accordionView.handleOpenSection = NO;
+	//[self.accordionView selectSection:1 animation:AccordionAnimationRight];
+	//self.accordionView.headerHeight = 30;
+	[self.accordionView load];
+}
+
+#pragma mark - AccordionViewDelegate
+
+- (NSArray *)accordionViewHeaders
+{
+	return @[@"First Section", @"Second Section", @"Third Section"];
+}
+
+- (NSArray *)accordionView:(AccordionTableView *)accordionView dataForHeaderInSection:(NSInteger)section
+{
+	switch (section) {
+		case 0:
+			return @[@"first"];
+			break;
+		case 1:
+			return @[@"second"];
+			break;
+		case 2:
+			return @[@"1", @"2", @"3"];
+			break;
+		default:
+			break;
+	}
+	return nil;
+}
+
+- (void)accordionView:(AccordionTableView *)accordionView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+}
+
+@end
